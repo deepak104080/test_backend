84ffc9c53253743bb3e1712517889c7f36840222
deepak104080 <deepak104080@gmail.com>
2023-09-14 21:07:42 +0530

    First Test API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
 const express = require('express');
 
 const app = express();
+
+
+
+
+
+app.get('/', (req, res) => {
+    try {
+        console.log('ExamHub API is running.........');
+        res.send('<h1>ExamHub API is running.........</h1>');
+    }
+    catch{
+
+    }
+})
+
+
+
+
+
+
+
+
+app.listen(4000, function() {
+    console.log('App running on port 4000.');
+    // db.connect(function(err) {
+    //     if(err) {
+    //         console.log('db connection error', err);
+    //     }
+    // })
+
+});
